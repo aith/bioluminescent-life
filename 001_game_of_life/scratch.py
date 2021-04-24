1215d390519b61d795233af4e9d9d03eddea84d3
aith <ariiramanesh@gmail.com>
2021-04-23 23:01:59 -0700

    took taichi example and added persistent impulse sources

--- 001_game_of_life/scratch.py
+++ 001_game_of_life/scratch.py
@@ -1,38 +1,31 @@
 import taichi as ti
 import numpy as np
-import clingo as cl
-ti.init()
 
+
+
+s = 10  # cell size
+numrow = 10  # num cells per row
+numcol = 20 # cells per col
+shape = (numrow, numcol)
+npa = np.random.randint(low=0, high= 2, size=shape)
+a = ti.field(dtype=ti.f32, shape=shape)
+a.from_numpy(npa)
+
+
+# what
 @ti.kernel
-def init():
+def paint():
     pass
-    # for i, j in alive:
-    #     if ti.random() > 0.8:
-    #         alive[i, j] = 1
-    #     else:
-    #         alive[i, j] = 0
 
 
-img_size = 800
-gui = ti.GUI('Title', (img_size, img_size)); gui.fps_limit = 15
-# cell_size = gui.slider('cell size', 0.0, 255.0, 1.0); cell_size.value = 2.0
+canw = s * numrow
+canl = s * numcol
+imgw = canw
+imgl = canl
+res = (600, 600)
 
-n = img_size
-grid = ti.field(int, shape=(n, n))
-init()
+gui = ti.GUI('title', res)
 while gui.running:
-    # for e in gui.get_events(gui.PRESS, gui.MOTION):
-    #     if e.key == gui.ESCAPE:
-    #         gui.running = False
-    #     elif e.key == gui.SPACE:
-    #         pass
-    # if gui.is_pressed(gui.LMB, gui.RMB):
-    #     # mx, my = gui.get_cursor_pos()
-    #     # alive[int(mx * n), int(my * n)] = gui.is_pressed(gui.LMB)
-    #     # paused = True
-    #     pass
-    # if not paused:
-    #     run()
-
-    gui.set_image(ti.imresize(grid, img_size).astype(np.uint8) * 255)
+    a_resized = ti.imresize(a, *res).astype(np.uint8) * 1.0
+    gui.set_image(a_resized)
     gui.show()
